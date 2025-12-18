11990d7e1ec0ff2652c1695bd1adce2117f6c30b
小阿狼是也 <Y_ALANG@outlook.com>
2025-12-19 06:15:40 +0800

    Update ads.tsx

--- config/ads.tsx
+++ config/ads.tsx
@@ -16,7 +16,7 @@ export const adsConfig: AdItem[] = [
     name: "雨云",
     text: "服务器厂商，提供高可用、高安全、高性能的服务器服务。",
     icon: <Icon icon="ri:links-fill" color="#4285F4" className="font-bold w-[1.2rem] h-[1.2rem]" />,
-    url: "https://url.al01.cn/ry",
+    url: "https://link.al01.cn/ryy",
     type: "url",
     mode: "to"
   },
@@ -24,7 +24,7 @@ export const adsConfig: AdItem[] = [
     name: "123pan",
     text: "云盘服务，提供高可用、高安全、高性能的云盘服务。",
     icon: <Icon icon="ri:links-fill" color="#4285F4" className="font-bold w-[1.2rem] h-[1.2rem]" />,
-    url: "https://url.al01.cn/123pan",
+    url: "https://link.al01.cn/123pan",
     type: "url",
     mode: "to"
   },
@@ -32,7 +32,7 @@ export const adsConfig: AdItem[] = [
     name: "阿里云ESA",
     text: "边缘加速服务，限时活动",
     icon: <Icon icon="ri:links-fill" color="#4285F4" className="font-bold w-[1.2rem] h-[1.2rem]" />,
-    url: "https://url.al01.cn/esa",
+    url: "https://link.al01.cn/esa",
     type: "url",
     mode: "to"
   },
@@ -40,7 +40,7 @@ export const adsConfig: AdItem[] = [
     name: "Flowith",
     text: "flowith 旨在提供一种超越传统对话的与人工智能互动的新方式。",
     icon: <Icon icon="ri:links-fill" color="#4285F4" className="font-bold w-[1.2rem] h-[1.2rem]" />,
-    url: "https://url.al01.cn/flowith",
+    url: "https://link.al01.cn/flowith",
     type: "url",
     mode: "to"
   },
@@ -48,7 +48,7 @@ export const adsConfig: AdItem[] = [
     name: "Qoder",
     text: "面向真实软件的智能体编程平台。",
     icon: <Icon icon="ri:links-fill" color="#4285F4" className="font-bold w-[1.2rem] h-[1.2rem]" />,
-    url: "https://url.al01.cn/qoder",
+    url: "https://link.al01.cn/qoder",
     type: "url",
     mode: "to"
   }
